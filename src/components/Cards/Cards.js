01d87f63108396b818cb2f53eb3d7a85dccaedb6
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 14:09:26 +0100

    Data is showing in each Card component for deaths, recovered and confirmed cases

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,8 +3,10 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
-console.log(props);
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
+  if(!confirmed) {
+      return "Loading..."
+  }
 
     return(
         <div className={styles.container}>
@@ -12,20 +14,20 @@ console.log(props);
               <Grid item component={Card}>
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                     <Typography variant="h5">Data Fetched</Typography>
+                     <Typography variant="h5">{confirmed.value}</Typography>
                      <Typography color="textSecondary">Dates Fetched</Typography>
                      <Typography variant="body2">Number of active cases</Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card}>
                   <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                  <Typography variant="h5">Data fetched</Typography>
+                  <Typography variant="h5">{recovered.value}</Typography>
                   <Typography color="textSecondary">Dates fetches</Typography>
                   <Typography variant="body2">Number of recovered cases</Typography>
               </Grid>
               <Grid item component={Card}>
                   <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                  <Typography variant="h5">Data Fetched</Typography>
+                  <Typography variant="h5">{deaths.value}</Typography>
                   <Typography color="textSecondary">Dates fetched</Typography>
                   <Typography variant="body2">Number of deaths</Typography>
               </Grid>
