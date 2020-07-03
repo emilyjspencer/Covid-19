e4f0d7c27e99dbb1b76bcd2f7e91f4decb97c9bf
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 01:40:04 +0100

    Exploring the data that is fetched from the API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 import { Cards, Chart, CountrySelector } from './components';
 
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+
+    }
+
+
     render() {
         return (
             <div className={styles.container}>
