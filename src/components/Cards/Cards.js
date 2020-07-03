1cf7e7b2187a60e495dee9b31f64816458ee304b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 15:34:32 +0100

    Adds styling to the cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
+import cx from 'classnames';
 import styles from './Cards.module.css';
 import CountUp from 'react-countup';
 
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
   if(!confirmed) {
-      return "Loading..."
+      return "Loading data..."
   }
 
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-              <Grid item component={Card}>
+              <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Infected</Typography>
                      <Typography variant="h5">
@@ -22,7 +23,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                      <Typography variant="body2">Number of active cases</Typography>
                      </CardContent> 
               </Grid>
-              <Grid item component={Card}>
+              <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)} >
                   <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                   <Typography variant="h5">
                       <CountUp start={0} end={recovered.value} duration={2.5} separator=","></CountUp>
@@ -30,7 +31,7 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                   <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                   <Typography variant="body2">Number of recovered cases</Typography>
               </Grid>
-              <Grid item component={Card}>
+              <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                   <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                   <Typography variant="h5">
                       <CountUp start={0} end={deaths.value} duration={2.5} separator="."></CountUp>
