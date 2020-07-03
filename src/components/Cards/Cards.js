da15cdc560df5c60dc155ced7551005019a96bd4
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-03 14:35:02 +0100

    Refactors to use the CountUp component to enable animations - showing the increase of cases/deaths/recoveries from a starting point of 0. Shows date of last update.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
+import CountUp from 'react-countup';
 
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
   if(!confirmed) {
@@ -14,21 +15,27 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
               <Grid item component={Card}>
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                     <Typography variant="h5">{confirmed.value}</Typography>
-                     <Typography color="textSecondary">Dates Fetched</Typography>
+                     <Typography variant="h5">
+                         <CountUp start={0} end={confirmed.value}  duration={2.5} separator=","></CountUp>
+                            </Typography>
+                     <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                      <Typography variant="body2">Number of active cases</Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card}>
                   <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                  <Typography variant="h5">{recovered.value}</Typography>
-                  <Typography color="textSecondary">Dates fetches</Typography>
+                  <Typography variant="h5">
+                      <CountUp start={0} end={recovered.value} duration={2.5} separator=","></CountUp>
+                  </Typography>
+                  <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                   <Typography variant="body2">Number of recovered cases</Typography>
               </Grid>
               <Grid item component={Card}>
                   <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                  <Typography variant="h5">{deaths.value}</Typography>
-                  <Typography color="textSecondary">Dates fetched</Typography>
+                  <Typography variant="h5">
+                      <CountUp start={0} end={deaths.value} duration={2.5} separator="."></CountUp>
+                  </Typography>
+                  <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                   <Typography variant="body2">Number of deaths</Typography>
               </Grid>
             </Grid>
