12859ddc72186a59d67a40bced2daeb8d1d8d2c3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-04 01:37:35 +0100

    Adds initial CountrySelector component and css

--- src/components/CountrySelector/CountrySelector.js
+++ src/components/CountrySelector/CountrySelector.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountrySelector.module.css';
 
 const CountrySelector = () => {
 
     return(
-        <h1></h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+                <h1>Test</h1>
+            </NativeSelect>
+        </FormControl>
     )
 }
 
