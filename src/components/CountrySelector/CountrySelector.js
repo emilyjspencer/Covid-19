dfe6599ddaec1e6bd0d293bb4d352e5f5db2bb46
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-06 16:58:02 +0100

    Renders drop down list in the CountrySelector component with 166 countries

--- src/components/CountrySelector/CountrySelector.js
+++ src/components/CountrySelector/CountrySelector.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
+import { fetchCountries } from '../../api';
 
 import styles from './CountrySelector.module.css';
 
 const CountrySelector = () => {
 
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+            fetchApi();
+        }, [setFetchedCountries]);
+
+        console.log(fetchedCountries);
+    
+
     return(
         <FormControl className={styles.formControl}>
             <NativeSelect>
                 <option value="global">Global</option>
+                {fetchedCountries.map((country, index) => <option key={index} value={country}>{country}</option>)}
                 <h1>Test</h1>
             </NativeSelect>
         </FormControl>
